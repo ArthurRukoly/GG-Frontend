7797bbcb11366181e6fb0c921bec29023f646be7
Arthur Rukoly <arthurrukoly000@gmail.com>
2023-10-21 02:06:29 +0300

    notpref product

--- FillPage/script.js
+++ FillPage/script.js
@@ -1,6 +1,5 @@
-var selectedProducts = [];
 
-function showSuggestions() {
+function showSuggestions1() {
     var input = document.getElementById("productName");
     var filter = input.value.toLowerCase();
     var suggestionList = document.getElementById("suggestionList");
@@ -12,7 +11,7 @@ function showSuggestions() {
             var suggestionItem = document.createElement("div");
             suggestionItem.textContent = productList[i].name;
             suggestionItem.classList.add("suggestion"); // Add a class for styling
-            suggestionItem.addEventListener("click", function() {
+            suggestionItem.addEventListener("click", function () {
                 // Set the input field's value to the clicked suggestion
                 input.value = this.textContent;
                 // Clear the suggestion list
@@ -23,47 +22,83 @@ function showSuggestions() {
     }
 }
 
-        function addSelectedProduct() {
-    var input = document.getElementById("productName");
+
+function showSuggestions2() {
+    var input = document.getElementById("NotPrefProductName");
+    var filter = input.value.toLowerCase();
+    var suggestionList = document.getElementById("suggestionList");
+    suggestionList.innerHTML = "";
+
+    for (var i = 0; i < productList.length; i++) {
+        var product = productList[i].name.toLowerCase();
+        if (product.startsWith(filter)) {
+            var suggestionItem = document.createElement("div");
+            suggestionItem.textContent = productList[i].name;
+            suggestionItem.classList.add("suggestion"); // Add a class for styling
+            suggestionItem.addEventListener("click", function () {
+                // Set the input field's value to the clicked suggestion
+                input.value = this.textContent;
+                // Clear the suggestion list
+                suggestionList.innerHTML = "";
+            });
+            suggestionList.appendChild(suggestionItem);
+        }
+    }
+}
+
+var selectedProducts = [];
+var selectedNotPrefProducts = [];
+
+function addSelectedProduct(isPreferred) {
+    var inputId = isPreferred ? "productName" : "NotPrefProductName";
+    var productList = isPreferred ? selectedProducts : selectedNotPrefProducts;
+
+    var input = document.getElementById(inputId);
     var selectedProductName = input.value.trim();
 
     if (selectedProductName.length > 0) {
-        selectedProducts.push(selectedProductName);
-        updateSelectedProductsList();
+        productList.push(selectedProductName);
+        updateSelectedProductsList(isPreferred);
         input.value = ""; // Clear the input field
     }
 }
 
-function updateSelectedProductsList() {
-    var selectedProductList = document.getElementById("selectedProductList");
-    selectedProductList.innerHTML = "";
+function updateSelectedProductsList(isPreferred) {
+    var productListId = isPreferred ? "selectedProductList" : "selectedNotPrefedProductList";
+    var productList = isPreferred ? selectedProducts : selectedNotPrefProducts;
 
-    for (var i = 0; i < selectedProducts.length; i++) {
-        var product = selectedProducts[i];
+    var productListElement = document.getElementById(productListId);
+    productListElement.innerHTML = "";
+
+    for (var i = 0; i < productList.length; i++) {
+        var product = productList[i];
         var listItem = document.createElement("li");
         listItem.classList.add("selected-product");
         listItem.textContent = product;
 
         var deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click", function() {
+        deleteButton.addEventListener("click", function () {
             // Remove the selected product from the array and update the list
-            var index = selectedProducts.indexOf(product);
+            var index = productList.indexOf(product);
             if (index !== -1) {
-                selectedProducts.splice(index, 1);
-                updateSelectedProductsList();
+                productList.splice(index, 1);
+                updateSelectedProductsList(isPreferred);
             }
         });
 
         listItem.appendChild(deleteButton);
-        selectedProductList.appendChild(listItem);
+        productListElement.appendChild(listItem);
     }
 }
+
 
 // Update the hidden input field with the selectedProducts array
 function updateHiddenInput() {
     var selectedProductsInput = document.getElementById("selectedProductsInput");
+    var selectedNotPrefProductsInput = document.getElementById("selectedNotPrefProductsInput");
     selectedProductsInput.value = JSON.stringify(selectedProducts);
+    selectedNotPrefProductsInput.value = JSON.stringify(selectedNotPrefProducts);
 }
 
 // Call updateHiddenInput() whenever the selectedProducts array changes
